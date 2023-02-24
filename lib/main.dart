d3ed4fa4d8f6d622e507069e0b8c1d94b394c715
Rishabh Sharma <rishabhsharma28302@gmail.com>
2023-02-24 18:55:31 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Homepage(),
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
